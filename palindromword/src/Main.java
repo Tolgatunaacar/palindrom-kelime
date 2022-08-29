af5c7e29b4b2e84ad7062d1d2ebcfbe18e8a41ca
Tolgatunaacar <110130709+Tolgatunaacar@users.noreply.github.com>
2022-08-29 20:33:19 +0300

    Add files via upload

---
+++ palindromword/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+         static Scanner scan = new Scanner(System.in);
+
+        public static boolean isPalindrome(String str) {
+            int i = 0;
+            int j = str.length() - 1;
+            while (i <= j) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    return true;
+                }
+                i++;
+                j--;
+            }
+            return false;
+        }
+
+
+        public static void main(String[] args) {
+
+            String word;
+            System.out.print("Denemek İstenilen Kelimeyi Giriniz: ");
+            word = scan.nextLine();
+            if (isPalindrome(word)) {
+                System.out.println(word + " bir palindrom kelime.");
+            } else {
+                System.out.println(word + " bir palindrom kelime değil!.");
+            }
+        }
+}
+
